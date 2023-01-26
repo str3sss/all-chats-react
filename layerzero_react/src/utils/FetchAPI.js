e0f507df4f3b8f1a382aae5304972b7240861b8f
Dmitry <punkreatine@gmail.com>
2023-01-26 12:05:57 +0300

    add: layerzero, optimism

---
+++ layerzero_react/src/utils/FetchAPI.js
@@ -0,0 +1,23 @@
+import { id } from "./getId"
+
+const api_url = 'http://65.109.173.32:1000/chatbot'
+
+export async function FetchAPI(question) {
+  let data = {user_id: id, question, network: "layerzero"}
+  try {
+    const response = await fetch("http://65.109.173.32:1000/chatbot", {
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      method: "POST"
+    })
+    const res = await response.json()
+    const result = await res[res.length - 1][1]
+    return result
+  } catch (e) {
+    console.log(e)
+    return 'Something went wrong'
+  }
+}
